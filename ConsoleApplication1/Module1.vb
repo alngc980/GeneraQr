cb508eb29843ea5fcee19c72c40d5cee5758ed38
Alan Andres Garcia Castillo <alngc980@gmail.com>
2023-08-06 18:38:12 -0500

    Modificado

--- ConsoleApplication1/Module1.vb
+++ ConsoleApplication1/Module1.vb
@@ -2,38 +2,88 @@
 Imports System.IO
 Imports System.Drawing
 Imports System.Windows
+Imports System.Data.SqlClient
 
 Module Module1
+    Public CadenaConexion As String = "Data Source=SERVER;Initial Catalog=SIGECO_PRUEBAS;User ID=sa;Password=123456"
+    Public Connection As New SqlConnection(CadenaConexion)
 
     Sub Main()
 
+        Dim dt As New DataTable
+        dt = RetornaDataTable("EXEC ListaQr")
 
+        If dt.Rows.Count > 0 Then
+            For i = 0 To dt.Rows.Count - 1
+                Dim Txt, NombreArch As String
 
+                Txt = dt.Rows(i)(0).ToString() + "|" + dt.Rows(i)(1).ToString() + "|" +
+                        dt.Rows(i)(2).ToString() + "|" + dt.Rows(i)(3).ToString() + "|" +
+                        dt.Rows(i)(4).ToString() + "|" + Convert.ToDateTime(dt.Rows(i)(5)).ToString("yyyy-MM-dd") + "|" +
+                        dt.Rows(i)(6).ToString() + "|" + dt.Rows(i)(7).ToString() + "|"
+                NombreArch = dt.Rows(i)(2).ToString() + "-" + dt.Rows(i)(3).ToString()
+                GenerarQr(Txt, NombreArch)
+            Next
+        End If
 
-        Console.WriteLine("Ingresa el contenido del QR:")
-        Dim qrContent As String = Console.ReadLine()
+    End Sub
+    Sub GenerarQr(Texto As String, NombreArch As String)
+        Dim qrContent As String = Texto
 
-        ' Generar el QR
         Dim qrGenerator As New QRCodeGenerator()
         Dim qrCodeData As QRCodeData = qrGenerator.CreateQrCode(qrContent, QRCodeGenerator.ECCLevel.Q)
         Dim qrCode As New QRCode(qrCodeData)
 
-        ' Crear el objeto de imagen del QR
         Dim qrImage As Bitmap = qrCode.GetGraphic(10) ' 10 es el tamaño del módulo del QR en píxeles
 
-        ' Ruta de destino para guardar la imagen
-        Dim rutaDestino As String = AppDomain.CurrentDomain.BaseDirectory + "\QR\qr.jpg"
+        Dim rutaDestino As String = AppDomain.CurrentDomain.BaseDirectory + "\QR\" & NombreArch & ".jpg"
 
         ' Guardar el QR en la ruta especificada
         Try
             qrImage.Save(rutaDestino, System.Drawing.Imaging.ImageFormat.Png)
-            Console.WriteLine("QR guardado correctamente en: " & rutaDestino)
         Catch ex As Exception
-            Console.WriteLine("Error al guardar el QR: " & ex.Message)
         End Try
-
-        Console.WriteLine("Presiona cualquier tecla para salir.")
-        Console.ReadKey()
     End Sub
+
+
+    Public Function AbrirConexion() As Boolean
+        Dim band As Boolean = False
+        Try
+            Connection.Open()
+            band = True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Return band
+    End Function
+    Public Function CerrarConexion() As Boolean
+        Dim band As Boolean = False
+        Try
+            Connection.Close()
+            band = True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Return band
+    End Function
+
+    Public Function RetornaDataTable(Query As String) As DataTable
+        Dim dt As New DataTable
+        Try
+            If AbrirConexion() Then
+                Dim comand As SqlCommand
+                Dim sda As New SqlDataAdapter
+                comand = New SqlCommand(Query, Connection)
+                sda = New SqlDataAdapter(comand)
+                sda.Fill(dt)
+            End If
+
+        Catch ex As Exception
+
+        Finally
+            CerrarConexion()
+        End Try
+        Return dt
+    End Function
 
 End Module
